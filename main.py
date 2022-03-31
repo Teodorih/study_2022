ed87ac563f93d82e08d9693a61e7629d68191aea
Victor Ivanpv <v.a.ivanov@corp.mail.ru>
2022-03-31 14:26:10 +0300

    our first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+string1 = 'spam/ '
+string2 = 'eggs'
+concat_string = string1 + string2
+print(len(concat_string))
+
+print(concat_string[::-1])
+
+print(concat_string.find('ddam'))
+
+print(concat_string.split('/'))
+
+array_string = concat_string.split('/')
+print("+".join(array_string))
+
+
+first_name = 'Victor'
+last_name = 'Petrov'
+new_string = f'Hello, {first_name} {last_name}'
+print(new_string)
