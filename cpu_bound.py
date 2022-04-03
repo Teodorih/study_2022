516e7aad2435da313e0d6de4cb8e9f06fc174b2c
Teodorih <Teodorih@hotmail.com>
2022-04-03 17:13:29 +0300

    try cpu_bound with asyncio and threading

--- cpu_bound.py
+++ cpu_bound.py
@@ -1,19 +1,29 @@
+import asyncio
+import concurrent.futures
 import time
 
 
-def cpu_bound(number):
+async def cpu_bound(number):
     return sum(i * i for i in range(number))
 
 
-def find_sums(numbers):
+async def find_sums(numbers):
+    tasks = []
     for number in numbers:
-        cpu_bound(number)
+        tasks.append(cpu_bound(number))
+    await asyncio.gather(*tasks)
+
+
+def new_find_sums(number):
+    return sum(i * i for i in range(number))
 
 
 if __name__ == "__main__":
     numbers = [5_000_000 + x for x in range(20)]
 
     start_time = time.time()
-    find_sums(numbers)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(new_find_sums, numbers)
+    #asyncio.run(find_sums(numbers))
     duration = time.time() - start_time
     print(f"Duration {duration} seconds")
